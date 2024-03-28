06db2cc429b386a707084e9cdae652cc6db301e5
Naval Shah <navalmaulikshah@gmail.com>
2024-03-28 18:13:07 -0400

    Save + Color functionality SHREK

--- app.py
+++ app.py
@@ -7,10 +7,19 @@ cv2.resizeWindow("meme_window", 1000, 500)
 image = cv2.imread("blinking_guy.jpg")
 print(image.shape)
 
-cv2.putText(image, "THIS iS MEMEE Text", (50, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,0), 15)
-cv2.putText(image, "THIS iS MEMEE Text", (50, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 7)
+
+#adding text: cv2.putText(image, text, (x, y), font, font_scale, color, line_thickness)
+cv2.putText(image, "Shrek.", (50, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,0), 15)
+cv2.putText(image, "Shrek.", (50, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 7)
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
 
 
 
 cv2.imshow("meme_window", image)
 cv2.waitKey(0)
+
+question = input("Do you wan to save your image? ")
+if question.lower() == "yes":
+    name=input("name you meme") + ".jpg"
+    cv2.imwrite(name, image)
